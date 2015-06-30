6bf6622a62bd53df7d5218c22fe4f9c6706eb44b
waltbai <3120000091@zju.edu.cn>
2015-06-30 18:54:42 +0800

    a version.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 bison compiler.y
 flex compiler.l
-g++ SymbolTable.c SyntaxTree.c TypeChecking.c MiddleWare.cpp node.cpp AST.cpp main.cpp -o test -Iinclude -I. `llvm-config --cppflags --libs core jit native --ldflags all-targets asmparser` -ltinfo -ldl -std=c++11
+g++ SymbolTable.c SyntaxTree.c TypeChecking.c MiddleWare.cpp node.cpp AST.cpp main.cpp -o test -Iinclude -I. `llvm-config --cppflags --libs core jit native --ldflags all-targets asmparser` -ltinfo -ldl -lpthread -std=c++11
 ./test test.in test.out
 
